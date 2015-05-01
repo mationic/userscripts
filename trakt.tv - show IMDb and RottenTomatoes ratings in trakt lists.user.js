dcd4eb52b0064360f00e887b7332841c806650b5
mationic <mationic@users.noreply.github.com>
2015-05-01 19:42:54 +0200

    added css

--- trakt.tv - show IMDb and RottenTomatoes ratings in trakt lists.user.js
+++ trakt.tv - show IMDb and RottenTomatoes ratings in trakt lists.user.js
@@ -4,8 +4,6 @@
 //
 // @description	   Loads ratings from omdbapi.com
 //
-// @include        /^https?://(.+\.)?trakt\.tv/(.+/)?lists/.*$/
-//
 // @include        /^https?://(.+\.)?trakt\.tv/(.+/)?(lists|watchlist|collection)/?.*$/
 //
 // @grant          GM_xmlhttpRequest
@@ -14,6 +12,9 @@
 // ==/UserScript==
 
 $(function() {
+
+    $('head').append('<style>.ratings { padding-left: 5px!important; background-color: white; color: black; font-size: 12px!important; text-align: left!important; };</style>');
+    $('head').append('<style>.value { padding-left: 10px!important; font-weight: bolder!important; font-size: 13px!important; };</style>');
 
     $('div[data-type="movie"]').each(function(i) {
 
@@ -29,13 +30,15 @@ $(function() {
                     onload: function(json) {
                         var res = $.parseJSON(json.responseText);
                         var h = $('<h4>', {
-                            'html': 'IMDb: ' + res.imdbRating + ' (' + res.imdbVotes + ' Votes)'
+                            'class': 'ratings',
+                            'html': 'IMDb: <span class="value">' + res.imdbRating + ' (' + res.imdbVotes + ' Votes)</span>'
                         });
                         var h2 = $('<h4>', {
-                            'html': 'RT (c/u): ' + res.tomatoRating.replace("N/A", "-") + ' / ' + res.tomatoUserRating.replace("N/A", "-")
+                            'class': 'ratings',
+                            'html': 'RT (c/u): <span class="value">' + res.tomatoRating.replace("N/A", "-") + ' / ' + res.tomatoUserRating.replace("N/A", "-") + '</span>'
                         });
-                        $(movie).find('.titles').prepend(h2);
-                        $(movie).find('.titles').prepend(h);
+                        $(movie).find('.quick-icons').after(h2);
+                        $(movie).find('.quick-icons').after(h);
 
                     }
                 });
