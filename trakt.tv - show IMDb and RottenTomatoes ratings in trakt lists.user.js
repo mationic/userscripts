33834a6485edf565373233c1249e19335eb5b7c5
mationic <mationic@users.noreply.github.com>
2015-05-02 05:48:03 +0200

    stable version

--- trakt.tv - show IMDb and RottenTomatoes ratings in trakt lists.user.js
+++ trakt.tv - show IMDb and RottenTomatoes ratings in trakt lists.user.js
@@ -1,72 +1,80 @@
 // ==UserScript==
-// @name           trakt.tv - show IMDb & RottenTomatoes ratings in trakt lists
+// @name           trakt.tv - add IMDb & RottenTomatoes movie ratings
 // @namespace      https://greasyfork.org/users/5174-jesuis-parapluis
 // @author         jesuis-parapluie
-// @description	   Loads ratings from omdbapi.com - (trakt.tv site changes currently need manual reload to activate script) 
+// @description	   Adds ratings from IMDb and RottenTomatoes to trakt
 //
-// @include        /^https?://(.+\.)?trakt\.tv/(.+/)?(lists|watchlist|collection|ratings|history)/?.*$/
-// @exclude        /^https?://(.+\.)?trakt\.tv/movies/.*$/
+// @include        /^https?://(.+\.)?trakt\.tv/?.*$/
 //
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 //
 // @grant          GM_xmlhttpRequest
 //
-// @version        0.0.3
+// @version        0.1
 //
 // ==/UserScript==
 
-$(function() {
+(function() {
+    'use strict';
+    var getRatingsForElement = function() {
 
-    $('head').append('<style>.ratings { padding-left: 10px!important; background-color: white; color: black; font-size: 12px!important; text-align: left!important; };</style>');
-    $('head').append('<style>.value { padding-left: 15px!important; font-weight: bolder!important; font-size: 13px!important; };</style>');
+        var imdb = $('<h4>', {
+            'class': 'ratings',
+            'html': 'IMDb: <span class="value">&nbsp;</span>'
+        });
+        var tomatoes = $('<h4>', {
+            'class': 'ratings',
+            'html': 'R.T. c/u: <span class="value">&nbsp;</span>'
+        });
+        $(this).find('.quick-icons').after(tomatoes);
+        $(this).find('.quick-icons').after(imdb);
 
-    $('div[data-type="movie"]').each(function(i) {
+        if ($(this).attr('data-type') == 'movie') {
 
-        var url = $(this).attr('data-url');
-        if (url) {
-            var movie = $(this);
-            $.get(url, function(data) {
-                var imdb = $(data).find('.external a:contains("IMDB")').attr('href').split('/').pop();
 
-                GM_xmlhttpRequest({
-                    method: "GET",
-                    url: "http://www.omdbapi.com/?plot=short&tomatoes=true&r=json&i=" + imdb,
-                    onload: function(json) {
-                        var res = $.parseJSON(json.responseText);
-                        if (typeof(res.imdbRating) == 'undefined' || res.imdbRating == "N/A") {
-                            res.imdbRating = '-   ';
-                            res.imdbVotes = 0;
+            var url = $(this).attr('data-url');
+            if (url) {
+                var movie = $(this);
+                $.get(url, function(data) {
+                    var imdb_id = $(data).find('.external a:contains("IMDB")').attr('href').split('/').pop();
+
+                    GM_xmlhttpRequest({
+                        method: "GET",
+                        url: "http://www.omdbapi.com/?plot=short&tomatoes=true&r=json&i=" + imdb_id,
+                        onload: function(json) {
+                            var res = $.parseJSON(json.responseText);
+                            if (typeof(res.imdbRating) == 'undefined' || res.imdbRating == "N/A") {
+                                res.imdbRating = '-   ';
+                                res.imdbVotes = 0;
+                            }
+                            if (typeof(res.tomatoRating) == 'undefined' || res.tomatoRating == "N/A") res.tomatoRating = '-';
+                            if (typeof(res.tomatoUserRating) == 'undefined' || res.tomatoUserRating == "N/A") res.tomatoUserRating = '-';
+                            $(imdb).find('span').html(res.imdbRating + ' (' + res.imdbVotes + ' Votes)');
+                            $(tomatoes).find('span').html('&nbsp;&nbsp;&nbsp;&nbsp;' + res.tomatoRating + ' / ' + res.tomatoUserRating + '</span>')
                         }
-                        if (typeof(res.tomatoRating) == 'undefined' || res.tomatoRating == "N/A") res.tomatoRating = '-';
-                        if (typeof(res.tomatoUserRating) == 'undefined' || res.tomatoUserRating == "N/A") res.tomatoUserRating = '-';
-                        var h = $('<h4>', {
-                            'class': 'ratings',
-                            'html': 'IMDb: <span class="value">' + res.imdbRating + ' (' + res.imdbVotes + ' Votes)</span>'
-                        });
-                        var h2 = $('<h4>', {
-                            'class': 'ratings',
-                            'html': 'R.T. c/u: <span class="value">&nbsp;&nbsp;&nbsp;&nbsp;' + res.tomatoRating + ' / ' + res.tomatoUserRating + '</span>'
-                        });
-                        $(movie).find('.quick-icons').after(h2);
-                        $(movie).find('.quick-icons').after(h);
+                    });
 
-                    }
                 });
-
-            });
+            }
 
         }
-    });
 
-    if ($('div[data-type="movie"]').size()) {
-        $('div[data-type="episode"]').each(function(i) {
-            var blank = $('<h4>', {
-                'class': 'ratings',
-                'html': '&nbsp;<span class="value">&nbsp;</span>'
-            });
-            $(this).find('.quick-icons').after(blank);
-            $(this).find('.quick-icons').after(blank.clone());
-        });
     }
 
-});
+    $(window).ready(function() {
+
+        $('head').append('<style>.ratings { padding-left: 10px!important; background-color: white; color: black; font-size: 12px!important; text-align: left!important; };</style>');
+        $('head').append('<style>.value { padding-left: 8px!important; font-weight: bolder!important; font-size: 13px!important; };</style>');
+        $('div.grid-item').each(getRatingsForElement);
+
+        $(window).bind('DOMNodeInserted', function(e) {
+
+            if (e.target.tagName == 'BODY') {
+                $(e.target).ready(function() {
+                    $('div.grid-item').each(getRatingsForElement);
+                });
+            }
+        });
+    });
+
+})();
