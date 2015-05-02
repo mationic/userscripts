d45dcfc317a4a39b7bf34fcb718ae0cfb3cbbca0
mationic <mationic@users.noreply.github.com>
2015-05-02 08:42:47 +0200

    imdb sorting

--- trakt.tv - show IMDb and RottenTomatoes ratings in trakt lists.user.js
+++ trakt.tv - show IMDb and RottenTomatoes ratings in trakt lists.user.js
@@ -11,7 +11,7 @@
 //
 // @grant          GM_xmlhttpRequest
 //
-// @version        0.1.1
+// @version        0.1.2
 //
 // ==/UserScript==
 
@@ -19,49 +19,68 @@
     'use strict';
     var getRatingsForElement = function() {
 
-        var imdb = $('<h4>', {
-            'class': 'ratings',
-            'html': 'IMDb: <span class="value">&nbsp;</span>'
-        });
-        var tomatoes = $('<h4>', {
-            'class': 'ratings',
-            'html': 'R.T. c/u: <span class="value">&nbsp;</span>'
-        });
-        $(this).find('.quick-icons').after(tomatoes);
-        $(this).find('.quick-icons').after(imdb);
+            var imdb = $('<h4>', {
+                'class': 'ratings',
+                'html': 'IMDb: <span class="value">&nbsp;</span>'
+            });
+            var tomatoes = $('<h4>', {
+                'class': 'ratings',
+                'html': 'R.T. c/u: <span class="value">&nbsp;</span>'
+            });
+            $(this).find('.quick-icons').after(tomatoes);
+            $(this).find('.quick-icons').after(imdb);
 
-        if ($(this).attr('data-type') == 'movie') {
+            if ($(this).attr('data-type') == 'movie') {
 
-            var url = $(this).attr('data-url');
+                var url = $(this).attr('data-url');
 
-            if (url) {
+                if (url) {
 
-                var movie = $(this);
-                $.get(url, function(data) {
-                    var imdb_id = $(data).find('.external a:contains("IMDB")').attr('href').split('/').pop();
+                    var movie = $(this);
+                    $.get(url, function(data) {
+                        var imdb_id = $(data).find('.external a:contains("IMDB")').attr('href').split('/').pop();
 
-                    GM_xmlhttpRequest({
-                        method: "GET",
-                        url: "http://www.omdbapi.com/?plot=short&tomatoes=true&r=json&i=" + imdb_id,
-                        onload: function(json) {
-                            var res = $.parseJSON(json.responseText);
-                            if (typeof(res.imdbRating) == 'undefined' || res.imdbRating == "N/A") {
-                                res.imdbRating = '-   ';
-                                res.imdbVotes = 0;
+                        GM_xmlhttpRequest({
+                            method: "GET",
+                            url: "http://www.omdbapi.com/?plot=short&tomatoes=true&r=json&i=" + imdb_id,
+                            onload: function(json) {
+                                var res = $.parseJSON(json.responseText);
+                                if (typeof(res.imdbRating) == 'undefined' || res.imdbRating == "N/A") {
+                                    res.imdbRating = '-   ';
+                                    res.imdbVotes = 0;
+                                }
+                                if (typeof(res.tomatoRating) == 'undefined' || res.tomatoRating == "N/A") res.tomatoRating = '-';
+                                if (typeof(res.tomatoUserRating) == 'undefined' || res.tomatoUserRating == "N/A") res.tomatoUserRating = '-';
+                                $(imdb).find('span').html(res.imdbRating + ' (' + res.imdbVotes + ' Votes)');
+                                $(tomatoes).find('span').html('&nbsp;&nbsp;&nbsp;&nbsp;' + res.tomatoRating + ' / ' + res.tomatoUserRating + '</span>')
                             }
-                            if (typeof(res.tomatoRating) == 'undefined' || res.tomatoRating == "N/A") res.tomatoRating = '-';
-                            if (typeof(res.tomatoUserRating) == 'undefined' || res.tomatoUserRating == "N/A") res.tomatoUserRating = '-';
-                            $(imdb).find('span').html(res.imdbRating + ' (' + res.imdbVotes + ' Votes)');
-                            $(tomatoes).find('span').html('&nbsp;&nbsp;&nbsp;&nbsp;' + res.tomatoRating + ' / ' + res.tomatoUserRating + '</span>')
-                        }
-                    });
+                        });
 
-                });
+                    });
+                }
+
             }
 
-        }
-
-    }
+        },
+        sortByRating = function() {
+            var dict = {};
+            $("div.grid-item").each(function() {
+                var reg = $(this).find("h4.ratings").text().match(/IMDb\:\W+(\d\.\d).*/i);
+                var rating = 0;
+                if (reg.length > 0) rating = reg[1];
+                if (dict[rating] === undefined) {
+                    dict[rating] = [$(this)];
+                } else {
+                    dict[rating].push($(this));
+                }
+            });
+            var order = Object.keys(dict).sort();
+            var parent = $("div.grid-item").parent();
+            while (order.length > 0) {
+                var items = rated[order.pop()];
+                while (items.length > 0) parent.append(items.pop());
+            }
+        };
 
     $(window).ready(function() {
 
@@ -69,10 +88,14 @@
         $('head').append('<style>.value { padding-left: 8px!important; font-weight: bolder!important; font-size: 13px!important; };</style>');
         if ($("div.grid-item[data-type='movie']").size() > 0) $('div.grid-item').each(getRatingsForElement);
 
+        $('.trakt-icon-list').click(sortByRating);
+
+
         $(window).bind('DOMNodeInserted', function(e) {
             if (e.target.tagName == 'BODY') {
                 $(e.target).ready(function() {
                     if ($("div.grid-item[data-type='movie']").size() > 0) $('div.grid-item').each(getRatingsForElement);
+                    $('.trakt-icon-list').click(sortByRating);
                 });
             }
         });
