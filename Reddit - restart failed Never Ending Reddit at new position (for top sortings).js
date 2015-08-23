eaacda9a62bcc21d9021841f4d4af185529dab27
mationic <mationic@users.noreply.github.com>
2015-08-23 08:09:50 +0200

    still unstable

--- Reddit - restart failed Never Ending Reddit at new position (for top sortings).js
+++ Reddit - restart failed Never Ending Reddit at new position (for top sortings).js
@@ -7,6 +7,7 @@
 // @updateURL      https://raw.githubusercontent.com/mationic/userscripts/master/Reddit%20-%20restart%20failed%20Never%20Ending%20Reddit%20at%20new%20position%20(for%20top%20sortings).js
 // @downloadURL    https://raw.githubusercontent.com/mationic/userscripts/master/Reddit%20-%20restart%20failed%20Never%20Ending%20Reddit%20at%20new%20position%20(for%20top%20sortings).js
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// @grant          none
 // @include        /^https?:\/\/(.+\.)?reddit\.com\/top/?.*$/
 // @exclude        /^https?:\/\/(.+\.)?reddit\.com\/.+\/comments\/.*$/
 // ==/UserScript==
