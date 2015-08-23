d8a812634b20cee337f8b1d80530ddc07f6a969d
mationic <mationic@users.noreply.github.com>
2015-08-23 07:19:55 +0200

    new script

---
+++ Reddit - restart failed Never Ending Reddit at new position (for top sortings).js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name           Reddit - restart failed Never Ending Reddit at new position (for top sortings)
+// @namespace      https://greasyfork.org/users/5174-jesuis-parapluie
+// @author         jesuis-parapluie
+// @version        0.0.1
+// @description    ..
+// @updateURL
+// @downloadURL
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// @include        /^https?:\/\/(.+\.)?reddit\.com\/top/?.*$/
+// @exclude        /^https?:\/\/(.+\.)?reddit\.com\/.+\/comments\/.*$/
+// ==/UserScript==
+
+
+(function () {
+    'use strict';
+    /*jslint browser:true, newcap:true */
+    /*global $, jQuery  */
+
+    var findLink = function () {
+        var link, trunk = $('div#NERFail>p.nextprev>a').last().prev().attr('href').split('t3_').shift(),
+            button = $('div#NERFail>p.nextprev>a#NERcontinue'),
+            commentIDs = [];
+        button.css('color','');
+        button.data("checkLinks", -1);
+        $('#siteTable').find('div.thing').each(function() {
+            commentIDs.push($(this).data('fullname'));
+        });
+        while (commentIDs.length > 0) {
+            link = trunk + commentIDs.pop();
+            button.text("Searching for working link (checked: "+(button.data("checkLinks")+1)+")");
+            $.get(href, function (data) {
+                if ($(data).find('#siteTable div.thing').size()) {
+                    break;
+                }
+            }
+        }
+        if (commentIDs.length === 0) {
+            button.text("Could'nt find a working link :(");
+        } else {
+            button.text("Found. Click here to continue browsing");
+            button.attr('href', link);
+            button.off('click');
+        }
+    }
+    $(function () {
+        $(document).bind('DOMNodeInserted', function (e) {
+            if (e.target.tagName === 'DIV' && e.target.getAttribute('id') && e.target.getAttribute('id') === 'NERFail') {
+                var button = $('<a>', {
+                     'text': 'find link and continue browsing',
+                     'id': 'NERcontinue',
+                     'href': 'javascript:;',
+                     'style': 'color:red;'
+                }).click(findLink);
+                $('div#NERFail>p.nextprev>a').first().after(button);
+            }
+        });
+    });
+
+}(jQuery));
