2895b5fb9ff2ab06fce2fec0bf83a4c1b1db7472
mationic <mationic@users.noreply.github.com>
2017-04-16 13:13:13 +0200

    Fixed RottenTomatoes

--- trakt.tv - show IMDb and RottenTomatoes ratings in trakt lists.user.js
+++ trakt.tv - show IMDb and RottenTomatoes ratings in trakt lists.user.js
@@ -2,7 +2,7 @@
 // @name           trakt.tv - add IMDb & RottenTomatoes movie ratings (and sorting options for ratings)
 // @namespace      https://greasyfork.org/users/5174-jesuis-parapluis
 // @author         jesuis-parapluie
-// @description    Inserts movie ratings from IMDb and RottenTomatoes into trakt and adds sorting options for ratings.
+// @description    Inserts movie ratings from IMDb, RottenTomatoes and Metacritic into trakt (with sorting options).
 //
 // @include        /^https?://(.+\.)?trakt\.tv/?.*$/
 // @exclude        /^https?://(.+\.)?trakt\.tv/(shows|calendars)/?.*$/
@@ -11,7 +11,7 @@
 //
 // @grant          GM_xmlhttpRequest
 //
-// @version        0.1.21
+// @version        0.1.22
 //
 // ==/UserScript==
 
@@ -25,11 +25,11 @@
     var loadRatingsForItem = function () {
             var imdb = $('<h3>', {
                     'class': 'ratings',
-                    'html': '<div style="float: left; width: 60px;">IMDb / Meta:</div><span class="value">&nbsp;</span>'
+                    'html': '<div style="float: left; width: 90px;">IMDb</div><span class="value">&nbsp;</span>'
                 }),
                 tomatoes = $('<h3>', {
                     'class': 'ratings',
-                    'html': '<div style="float: left; width: 65px;">R.T. c/u:</div><span class="value">&nbsp;</span>'
+                    'html': '<div style="float: left; width: 97px;">R.T. / Metascore</div><span class="value">&nbsp;</span>'
                 }),
                 dummy = $('<h3>', {
                     'class': 'ratings',
@@ -49,22 +49,24 @@
                             method: "GET",
                             url: "http://www.omdbapi.com/?plot=short&tomatoes=true&r=json&i=" + imdb_id,
                             onload: function (json) {
-                                var res = $.parseJSON(json.responseText);
+                                var r, res = $.parseJSON(json.responseText);
+                                for (r in res.Ratings) {
+                                    if (res.Ratings.hasOwnProperty(r) && res.Ratings[r].Source === "Rotten Tomatoes") {
+                                        res.tomatoRating = res.Ratings[r].Value;
+                                    }
+                                }
                                 if (res.tomatoRating === undefined || res.tomatoRating === "N/A") {
                                     res.tomatoRating = '-';
-                                }
-                                if (res.tomatoUserRating === undefined || res.tomatoUserRating === "N/A") {
-                                    res.tomatoUserRating = '-';
                                 }
                                 if (res.Metascore === undefined || res.Metascore === "N/A") {
                                     res.Metascore = '-';
                                 }
                                 if (res.imdbRating === undefined || res.imdbRating === "N/A") {
-                                    $(imdb).find('span.value').html('-' + ' / ' + res.Metascore);
+                                    $(imdb).find('span.value').html('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-');
                                 } else {
-                                    $(imdb).find('span.value').html('<a href="http://www.imdb.com/title/' + res.imdbID + '">' + res.imdbRating + '<span style="font-size: 11px!important; font-style: normal; color: #999;"> (' + res.imdbVotes + ')</span></a>' + ' / ' + res.Metascore);
+                                    $(imdb).find('span.value').html('<a href="http://www.imdb.com/title/' + res.imdbID + '">' + res.imdbRating + '<span style="font-size: 11px!important; font-style: normal; color: #999;"> (' + res.imdbVotes + ')</span></a>');
                                 }
-                                $(tomatoes).find('span.value').html(res.tomatoRating + ' / ' + res.tomatoUserRating + '</span>');
+                                $(tomatoes).find('span.value').html(res.tomatoRating + '<span style="font-size: 11px!important; font-style: normal; color: #999;"> &nbsp;/&nbsp; </span>' + res.Metascore);
                             },
                             onerror: function () {
                                 $(imdb).find('span.value').html('<span style="color: #c11!important; font-weight: normal; font-size: 12px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;failed<span>');
@@ -88,16 +90,13 @@
                 }
             }
             if (type === 'imdb') {
-                r = $(item).find("h3.ratings").text().match(/IMDb \/ Meta:\W+(\d(\.\d)?).*/i);
+                r = $(item).find("h3.ratings").text().match(/IMDb\W+(\d(\.\d)?).*/i);
+            }
+            if (type === 'tomatometer') {
+                r = $(item).find("h3.ratings").text().match(/R\.T\.[^\d]+(\d*)%?.*/i);
             }
             if (type === 'metascore') {
-                r = $(item).find("h3.ratings").text().match(/IMDb \/ Meta:[^\/]+\/\W*(\d+).*/i);
-            }
-            if (type === 'rtcritic') {
-                r = $(item).find("h3.ratings").text().match(/R\.T\. c\/u\:\W+(\d(\.\d)?)-?\W*\/.*/i);
-            }
-            if (type === 'rtuser') {
-                r = $(item).find("h3.ratings").text().match(/R\.T\. c\/u\:\W+\d?\.?\d?-?\W*\/\W*(\d(\.\d)?)-?.*/i);
+                r = $(item).find("h3.ratings").text().match(/R\.T\. \/[^\/]+\/\W*(\d+).*/i);
             }
             if (r !== null && r.length > 1 && r[1] !== '-') {
                 return r[1];
@@ -153,9 +152,8 @@
             if (/^\/users\/.+\/(collection|ratings|lists\/|watchlist)/.test(window.location.pathname) && $('.trakt-icon-swap-vertical').next().find('ul a.rating').size() === 0) {
                 var sortMenu = $('.trakt-icon-swap-vertical').next().find('ul');
                 sortMenu.append($('<li>', { html: "<a class='rating' data-sort-by='imdb' data-sort-how='desc'>IMDb rating</a>" }));
+                sortMenu.append($('<li>', { html: "<a class='rating' data-sort-by='tomatometer' data-sort-how='desc'>TomatoMeter</a>" }));
                 sortMenu.append($('<li>', { html: "<a class='rating' data-sort-by='metascore' data-sort-how='desc'>Metascore</a>" }));
-                sortMenu.append($('<li>', { html: "<a class='rating' data-sort-by='rtcritic' data-sort-how='desc'>RottenTomatoes critic</a>" }));
-                sortMenu.append($('<li>', { html: "<a class='rating' data-sort-by='rtuser' data-sort-how='desc'>RottenTomatoes user</a>" }));
                 sortMenu.find('a.rating').click(sortByRating);
                 sortMenu.find('a').click(setPositioning);
                 $(window).on('resize', setPositioning);
